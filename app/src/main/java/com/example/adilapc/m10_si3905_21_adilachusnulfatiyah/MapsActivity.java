29556218d2574ed252d551cb4c13b80a16c23d46
borongajayuk <borongajayuk@gmail.com>
2018-04-05 11:04:27 +0700

    push satu

---
+++ app/src/main/java/com/example/adilapc/m10_si3905_21_adilachusnulfatiyah/MapsActivity.java
@@ -0,0 +1,13 @@
+package com.example.adilapc.m10_si3905_21_adilachusnulfatiyah;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MapsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+    }
+}
